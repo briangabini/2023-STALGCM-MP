ecddc6cf6727a63570ed040f32945904f054b783
Brian Gabini <91449132+nriab-dlsu@users.noreply.github.com>
2023-08-02 13:30:02 +0800

    current progress

--- Machine.java
+++ Machine.java
@@ -3,6 +3,10 @@
  * 
  */
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -13,29 +17,133 @@ import java.util.Stack;
 public class Machine {
     private List<State> states; // Set of states 'Q'
     private List<Character> inputAlphabet; // Input alphabet 'sigma'
+    // maybe we need a stack alpabhet
+    private List<Character> stackAlphabet;
     private State initialState; // 'qI'
     private State finalState; // 'qF'
     private int currHead; // the current symbol being read in the input string
 
-    // constructor for the Machine, this will be used
+    // constructor for the Machine, this will be used in the gui
     public Machine(File file) throws CustomException {
 
+        /*
+         * inputs to read from the file in order
+         * 
+         * number of states
+         * state names
+         * number of inputs
+         * input symbols // lambda doesn't need to be explicitly defined '^'
+         * number of stack symbols
+         * stack symbols
+         * number of transitions
+         * transitions
+         * initial state
+         * initial stack symbol 'Z'
+         * final state
+         * 
+         */
+
         try {
-            Buffered fileReader = new BufferedReader(new FileReader(file));
+            Buffered reader = new BufferedReader(new FileReader(file));
 
-            int numStates = Integer.parseInt(fileReader.readLine().trim());
+            // read the number of states
+            int numStates = Integer.parseInt(reader.readLine().trim());
 
-            // Read each state
+            // read each state, the state names will be read and will be attached to each of
+            // the State object
+            String[] states = reader.readline().trim().split(" ");
 
-        } catch (error e) {
+            // check if the number of states is matching with the states provided
+            if (numStates <= 0 || (numStates != states.length)) {
+                throw new CustomException(
+                        "Invalid number of states or the number of states doesn't match the provided states");
+            }
 
+            this.states = new ArrayList<>();
+            for (int i = 0; i < numStates; i++) {
+                this.states.add(new State(states[i]));
+            }
+
+            // read the number of inputs
+            int numInputs = Integer.parseInt(reader.readLine().trim());
+
+            // iterate and read over every input state
+            String[] inputSymbols = reader.readLine().trim().split(" ");
+            this.inputAlphabet = new ArrayList<>();
+
+            for (int i = 0; i < inputSymbols.length; i++) {
+                if (inputSymbols[i].length() > 1 || (inputSymbols.length != numInputs)) {
+                    throw new CustomException("Invalid Input Symbols");
+                }
+                this.inputAlphabet.add(inputSymbols[i].chartAt[0]);
+            }
+
+            // read number of stack symbols
+            int numStackSymbols = Integer.parseInt(reader.readLine().trim());
+
+            // iterate over every stack symbol
+            String[] stackSymbols = reader.readLine().trim().split(" ");
+            this.stackAlphabet = new ArrayList<>();
+
+            for (int i = 0; i < stackSymbols.length; i++) {
+                if (stackSymbols[i].length() > 1 || stackSymbols.length != numStackSymbols) {
+                    throw new CustomException("Invalid stack symbols");
+                }
+                this.stackAlphabet.add(stackSymbols[i].charAt[0]);
+            }
+
+            // read no. of transitions
+            int numTransitions = Integer.parseInt(reader.readLine().trim());
+
+            // TODO: capture the error later if the number of transitions doesn't match with
+            // the provided transitions
+            // TODO: capture the error later if the number of input symbols doesn't match
+            // with the provided input symbols
+
+            // add transitions to each state
+            // sample transition
+            // q1 // a ^ ^ q2 ^ ^ R
+            for (int i = 0; i < numTransitions; i++) {
+                String[] transitions = reader.readline().trim().split(" ");
+
+                if (transition.length != 7) {
+                    throw new CustomException("Invalid transition.");
+                } 
+
+                State currentState = State.findStateByName(transitions[0], this.states);
+                State nextState = State.findByStateName(transitions[4], this.states);
+
+                // check if 
+                if (currentState != null && nextState != null && (this.inputAlphabet.contains(transitions[1])))
+            }
+
+            // read the initial state (starting)
+            State initialState = State.findStateByName(reader.readLine(), this.states);
+            if (initialState == null) {
+                throw new CustomException("Invalid Starting State");
+            }
+            // read the final state (accepting)
+            State acceptingState = State.findStateByName(reader.readLine(), this.states);
+            if (acceptingState == null) {
+                throw new CustomException("Invalid Accepting State");
+            }
+        } catch (Exception e) {
+            throw new CustomException("Invalid File: " + e);
         }
     }
 
-    public main(String[] args) {
-        // check if the files are properly taken 
-    }
+    /*
+     * public main(String[] args) {
+     * // check if the files are properly taken
+     * }
+     */
 
     // TODO: implement backtracking
+
+    /*
+     * public static checkValidTransition() {
+     * 
+     * }
+     */
 
 }
--- State.java
+++ State.java
@@ -12,5 +12,63 @@ public class State {
     private Map<Character, ArrayList<TransitionKey>> transitions; // map an input symbol to multiple transitions, this
                                                                   // is because of non-determinism input to transition
                                                                   // is 1..*
+                                                                  // e.g. input symbol 1 is mapped to multiple
+                                                                  // transitions
+
+    // constructor for the final states
+    public State(String name, boolean isFinalState) {
+        this.name = name;
+        this.isFinalState = isFinalState;
+        this.transitions = new HashMap<>();
+    }
+
+    // constructor for the non-final states
+    public State(String name) {
+        this.name = name;
+        this.isFinalState = false;
+        this.transitions = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // create a function to add transitions
+    public void addTransition(Character inputSymbol, TransitionKey transkey) {
+        // check if input symbol is not already in the map
+        // if it is update the arraylist
+
+        // checks if the hashmap contains the key 'inputSymbol', if it does, replace the
+        // current value 'ArrayList<TransitionKey> transitions',
+        // otherwise, insert directly
+        if (transitions.containsKey(inputSymbol)) {
+            ArrayList<TransitionKey> transitionArrList = new ArrayList<>();
+            transitionArrList = transitions.get(inputSymbol);
+            transitionArrList.add(transkey);
+
+            // update the key-value pair
+            transitions.put(inputSymbol, transitionArrList);
+
+        } else {
+            // input symbol is not in map
+            ArrayList<TransitionKey> newtransitionArrList = new ArrayList<>();
+            newtransitionArrList.add(transkey);
+            transitions.put(inputSymbol, transkey);
+        }
+    }
+
+    // this will be a helper function in order to locate the states in the list
+    public static State findStateByName(String stateName, List<State> states) {
+        for (int i = 0; i < states.length; i++) {
+            if (states.get(i).getName().equals(stateName)) {
+                return states.get(i);
+            }
+        }
+
+    }
 
 }
